e992f31dde4b4f3a838fc9a8522be506af667cbf
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2016-09-06 12:20:18 +0100

    Change of form for happiness demo, data callbacks and home page

--- app/pages/forms/forms.ts
+++ app/pages/forms/forms.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
-import {KoboApi} from "../../providers/kobo-api/kobo-api";
-import {Observable} from 'rxjs/Observable'
+
 
 
 @Component({
@@ -9,17 +8,9 @@ import {Observable} from 'rxjs/Observable'
 })
 
 export class FormsPage {
-  forms: any = [];
-  anyErrors: boolean;
-  finished: boolean;
-  private data: Observable<any>;
 
-  constructor(private koboApi:KoboApi) {
-    this.koboApi.koboRequest('https://kc.kobotoolbox.org/api/v1/forms?format=json').subscribe(
-      result =>this.forms = result,
-      error =>this.anyErrors=true,
-      () => this.finished = true
-    );
+  constructor() {
+
   }
 
 
