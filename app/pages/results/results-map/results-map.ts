98e1cf0464da26088816084023d7eb1d437b621f
chrismclarke <chris.m.clarke@live.co.uk>
2016-09-29 17:00:04 +0100

    Map clustering, change of map tiles

--- app/pages/results/results-map/results-map.ts
+++ app/pages/results/results-map/results-map.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 import {MapService} from "../../../providers/map-provider/map-provider";
+import {Storage, SqlStorage} from 'ionic-angular';
 
 //declare L to allow for awesome markers (couldn't figure out how to update typescript index)
 declare var L;
@@ -11,7 +12,9 @@ declare var L;
 })
 export class ResultsMapPage {
 
-    mapService:any;
+  mapService:any;
+  storage:Storage;
+  results:any;
 
   constructor(private nav: NavController, params:NavParams, mapService:MapService, public viewCtrl:ViewController) {
     this.mapService=mapService;
@@ -47,17 +50,19 @@ export class ResultsMapPage {
     //function to change online/offline map and give warning after zoom
     map.on('zoomend',function(){
       var z =map.getZoom();
-      if (z<8){this.zoomWarning=false}
-      if (z>=8){this.zoomWarning=true}
-      if (z>8){return baseMaps.Online.addTo(map) }
+      if (z<6){this.zoomWarning=false}
+      if (z>=6){this.zoomWarning=true}
+      if (z>6){return baseMaps.Online.addTo(map) }
       //bind this allows to update scope through external callback function
     }.bind(this));
 
-    //add markers from font-awesome collection
-    var redMarker = L.AwesomeMarkers.icon({
-      icon: 'shopping-cart',
-      markerColor: 'red',
-      prefix:'fa'
+    //get data and load map points
+    this.storage = new Storage(SqlStorage);
+    this.storage.get('results').then((results)=> {
+      //convert from cached string to json format, use data from form id 69280
+      let allResults=JSON.parse(results);
+      this.results=allResults[69280];
+      this.addMapPoints(map,this.results)
     });
 
     this.mapService.map = map;
@@ -65,4 +70,41 @@ export class ResultsMapPage {
   close(){
     this.viewCtrl.dismiss()
   }
+  addMapPoints(map,data){
+    console.log('adding map points');
+    console.log(data);
+    var markersClusterGroup=L.markerClusterGroup({
+      showCoverageOnHover:false,
+    });
+    for(let item of data){
+      //if gps recorded
+      if(item['_geolocation'][0]!=null){
+        var iconData = generateIconData(item);
+        var icon = L.AwesomeMarkers.icon(iconData);
+        var marker = L.marker(item['_geolocation'], {icon: icon});
+        var popupContent=
+            "<h3>"+item['what_s_your_name_']+"</h3>"
+            +"<p>"+item['would_you_like_to_add_a_comment_']+"</p>";
+        marker.bindPopup(popupContent);
+        markersClusterGroup.addLayer(marker);
+        /*marker.addTo(map);*/
+      }
+    }
+    map.addLayer(markersClusterGroup)
+  }
 }
+
+function generateIconData(item){
+  var icon, color;
+  if(item['how_do_you_feel_today_']=='Happy'){color='green';icon='smile-o';}
+  else if(item['how_do_you_feel_today_']=='Neutral'){color='orange';icon='meh-o';}
+  else if(item['how_do_you_feel_today_']=='Unhappy'){color='red';icon='frown-o';}
+  var iconData = {
+    icon:icon,
+    markerColor:color,
+    prefix:'fa'
+  };
+  return iconData
+}
+
+
