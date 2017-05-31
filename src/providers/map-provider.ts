c8eed0c8fe57042319e406046c6b47d2c80d2307
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2017-05-31 23:14:49 +0100

    more work on v3 upgrade

--- src/providers/map-provider.ts
+++ src/providers/map-provider.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {NavController, Platform} from 'ionic-angular';
 import  'leaflet';
 import  'leaflet.markercluster';
-import  'leaflet.awesome-markers';
+// import  'leaflet.awesome-markers';
 
 @Injectable()
 export class MapService {
